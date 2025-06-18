8fc8fb1e6e2d10089606a9be411c1f5118c05473
CMunzwa <cmunzwa@gmail.com>
2025-06-18 13:27:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2361,7 +2361,7 @@ def webhook():
                 
                 # If user is talking to a human agent, suppress bot
                 if user_data.get("step") == "talking_to_human_agent":
-                forward_message_to_agent(message_text, user_data, phone_id)
+                    forward_message_to_agent(message_text, user_data, phone_id)
                 return "OK"
 
                 

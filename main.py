2ccf4ca407f11f893e37b2f12d3c5ca1d2b70d7b
CMunzwa <cmunzwa@gmail.com>
2025-06-19 10:54:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def get_user_state(phone_number):
         return json.loads(state)
     return state
 
-def update_user_state(phone_number, updates, ttl_seconds=60):
+def update_user_state(phone_number, updates, ttl_seconds=3600):
     updates['phone_number'] = phone_number
     if 'sender' not in updates:
         updates['sender'] = phone_number

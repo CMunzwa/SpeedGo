fb7ccba895adf7ec9421fdf0d0c3c897d8b92faf
CMunzwa <cmunzwa@gmail.com>
2025-06-26 12:06:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,9 +27,7 @@ gen_api = os.environ.get("GEN_API")
 owner_phone = os.environ.get("OWNER_PHONE")
 GOOGLE_MAPS_API_KEY = "AlzaSyCXDMMhg7FzP|ElKmrlkv1TqtD3HgHwW50"
 AGENT_NUMBER = [
-    "+263779562095",
-    "+263779469216",  
-    "+263785913291",
+    "+263772210415",
     "+263719835124"
 ]
 
